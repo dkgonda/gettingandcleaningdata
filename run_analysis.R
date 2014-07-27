89f7e6bd0ca13d4ade480fdc9700e4fdb9c8d8de
David Keith Gonda <dkgonda@yahoo.com>
2014-07-27 17:03:39 -0400

    xxx

--- run_analysis.R
+++ run_analysis.R
@@ -8,31 +8,31 @@
 
 
 #read in the dataset files after unzipping archive in working directory
-activity_labels = read.table("./UCI HAR Dataset/activity_labels.txt")
-features = read.table("./UCI HAR Dataset/features.txt")
-subject_test = read.table("./UCI HAR Dataset/test/subject_test.txt")
-X_test = read.table("./UCI HAR Dataset/test/X_test.txt")
-y_test = read.table("./UCI HAR Dataset/test/y_test.txt")
-subject_train = read.table("./UCI HAR Dataset/train/subject_train.txt")
-X_train = read.table("./UCI HAR Dataset/train/X_train.txt")
-y_train = read.table("./UCI HAR Dataset/train/y_train.txt")
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+features <- read.table("./UCI HAR Dataset/features.txt")
+subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
 
 #combine test and train data pieces
-X_combined = rbind(X_train,X_test)
-subject_combined = rbind(subject_train,subject_test)
-y_combined = rbind(y_train,y_test)
+X_combined <- rbind(X_train,X_test)
+subject_combined <- rbind(subject_train,subject_test)
+y_combined <- rbind(y_train,y_test)
 
 #combine attributes
-all_combined = cbind(subject_combined,y_combined,X_combined)
+all_combined <- cbind(subject_combined,y_combined,X_combined)
 
 #initial name of attributes
 names(features) <- c("featureID","featureName")
 names(activity_labels) <- c("activityID","activityLabel")
-featureNames = as.vector(features[,"featureName"])
-names(all_combined) = c("subjectID","activityID",featureNames)
+featureNames <- as.vector(features[,"featureName"])
+names(all_combined) <- c("subjectID","activityID",featureNames)
 
 #restrict names to subject/training ids + mean and std attributes
-mean_std_data = all_combined[,grep("subjectID|activityID|mean|std",names(all_combined))]
+mean_std_data <- all_combined[,grep("subjectID|activityID|mean|std",names(all_combined))]
 
 #clean up attribute names
 temp1 <- gsub("\\(\\)","",names(mean_std_data))
@@ -52,7 +52,7 @@ write.csv(mean_std_data,"tidydata.csv",row.names=FALSE)
 #of subject and activity,
 #and alter names of frame to reflect new values;
 #append "avgBySubjectActivity" to all but first two columns
-agg_mean_std_data <- aggregate(.~subjectID+activityLabel,mean_std_data,mean)
+agg_mean_std_data <- aggregate(.~activityLabel+subjectID,mean_std_data,mean)
 n <- names(agg_mean_std_data)
 new_n <- c( n[1:2],paste("avgBySubjectActivity", n[3:length(n)],sep=""))
 names(agg_mean_std_data) <- new_n
